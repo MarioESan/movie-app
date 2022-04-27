b166b6d4ae86d80c427dde4748f745b3a6ffc486
Mario Sanchez <mario.sanchez@sciodev.com>
2022-04-27 02:55:06 -0500

    React pwa movie app

---
+++ build/precache-manifest.54070edd802922635a06287bfbef3dac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f60559a5a6345a7c286",
+    "url": "/static/css/main.c2022b33.chunk.css"
+  },
+  {
+    "revision": "6f60559a5a6345a7c286",
+    "url": "/static/js/main.8665cc46.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "267c695f53bbdd361712",
+    "url": "/static/css/2.0cc5688e.chunk.css"
+  },
+  {
+    "revision": "267c695f53bbdd361712",
+    "url": "/static/js/2.4817747b.chunk.js"
+  },
+  {
+    "revision": "b1eb838c2e77f4ad0963e005df5056e8",
+    "url": "/index.html"
+  }
+];
